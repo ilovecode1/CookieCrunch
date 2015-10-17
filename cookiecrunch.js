1bb50a15764cc7e32dc2ece140bfd25cfbc08402
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-10-17 19:53:31 -0400

    Update cookiecrunch.js

--- cookiecrunch.js
+++ cookiecrunch.js
@@ -1,3 +1,4 @@
+function eraseCookie(e){document.cookie=e+"=; Max-Age=0"}
 var cookies = document.cookie.split(";");
 for (var i = 0; i < cookies.length; i++)
 eraseCookie(cookies[i].split("=")[0]); 
